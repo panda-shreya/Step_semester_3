30de467fecbcbb19f8d978a7f1b8fb8fa5e08f87
pandaops307 <casetty8@gmail.com>
2026-09-05 14:32:09 +0530

    Add Arrays Category C assignment problems

---
+++ Arrays_and_Methods/assigment_problems/ProductOfArrayExceptSelf.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    static int[] productExceptSelf(int[] nums) {
+
+        int[] answer = new int[nums.length];
+
+        answer[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int rightProduct = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        System.out.println(
+            Arrays.toString(productExceptSelf(nums))
+        );
+    }
+}
